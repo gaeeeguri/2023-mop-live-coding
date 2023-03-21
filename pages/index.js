e83cc22aefb2f4f411687c9943f1365841f3a4e9
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-03-22 06:12:03 +0900

    style(index): remove animation

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import {
   Image,
   useColorModeValue,
 } from '@chakra-ui/react';
-import Section from '@/components/section';
 import NextLink from 'next/link';
 
 const Home = () => {
@@ -18,7 +17,7 @@ const Home = () => {
   };
 
   return (
-    <Container>
+    <Container mb={4}>
       <Box
         borderRadius='lg'
         bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
@@ -30,85 +29,93 @@ const Home = () => {
         Welcome to <b>MOP:DEV</b> courses!
       </Box>
 
-      <Section delay={0.1}>
-        <Box display={{ md: 'flex' }}>
-          <Box flexGrow={1}>
-            <Heading as='h2' variant='page-title'>
-              Courses
-            </Heading>
-          </Box>
-          <Box></Box>
+      <Box display={{ md: 'flex' }} mb={6}>
+        <Box flexGrow={1}>
+          <Heading as='h2' variant='page-title'>
+            Courses
+          </Heading>
         </Box>
-      </Section>
+        <Box></Box>
+      </Box>
 
-      <Section delay={0.2}>
+      <Box
+        maxW='container.md'
+        borderWidth='1px'
+        borderRadius='lg'
+        overflow='hidden'
+        display={{ base: 'block', sm: 'flex' }}
+        background={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        direction={{ base: 'row', sm: 'column' }}
+      >
+        <Image
+          src='reactjs.png'
+          alt={reactCourse.imageAlt}
+          objectFit='cover'
+          maxW={{ base: '100%', sm: '40%' }}
+        />
+
         <Box
-          maxW='container.md'
-          // maxH={{ base: 'sm' }}
-          borderWidth='1px'
-          borderRadius='lg'
-          overflow='hidden'
-          display={{ base: 'block', sm: 'flex' }}
-          background={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-          direction={{ base: 'row', sm: 'column' }}
+          p='4'
+          display='flex'
+          flexDirection='column'
+          justifyContent='space-between'
+          flexGrow={1}
         >
-          <Image
-            src='reactjs.png'
-            alt={reactCourse.imageAlt}
-            objectFit='cover'
-            maxW={{ base: '100%', sm: '40%' }}
-          />
-
-          <Box
-            p='4'
-            display='flex'
-            flexDirection='column'
-            justifyContent='space-between'
-            flexGrow={1}
-          >
-            <Box>
-              <Box display='flex' alignItems='baseline'>
-                <Badge borderRadius='full' px='2' colorScheme='teal' mr={1}>
-                  Web
-                </Badge>
-                <Badge borderRadius='full' px='2' colorScheme='purple'>
-                  JS
-                </Badge>
-                <Box
-                  color='gray.500'
-                  fontWeight='semibold'
-                  letterSpacing='wide'
-                  fontSize='xs'
-                  textTransform='uppercase'
-                  ml='2'
-                >
-                  for beginners
-                </Box>
-              </Box>
+          <Box>
+            <Box display='flex' alignItems='baseline'>
+              <Badge borderRadius='full' px='2' colorScheme='teal' mr={1}>
+                Web
+              </Badge>
+              <Badge borderRadius='full' px='2' colorScheme='purple'>
+                JS
+              </Badge>
               <Box
-                mt='1'
+                color='gray.500'
                 fontWeight='semibold'
-                as='h4'
-                lineHeight='tight'
-                noOfLines={1}
+                letterSpacing='wide'
+                fontSize='xs'
+                textTransform='uppercase'
+                ml='2'
               >
-                {reactCourse.title}
+                for beginners
               </Box>
             </Box>
-            <Box display={'flex'} justifyContent={'flex-end'}>
-              <Button
-                background={useColorModeValue('#f0e7db', '#202023')}
-                mr={0}
-                size='sm'
-                as={NextLink}
-                href='/course/counting-numbers'
-              >
-                Go to Course
-              </Button>
+            <Box
+              mt='1'
+              fontWeight='semibold'
+              as='h4'
+              lineHeight='tight'
+              noOfLines={1}
+            >
+              {reactCourse.title}
             </Box>
           </Box>
+          <Box display={'flex'} justifyContent={'flex-end'} mt={3}>
+            <Button
+              // background={useColorModeValue('#f0e7db', '#202023')}
+              mr={0}
+              size={{ base: 'sm', md: 'md' }}
+              as={NextLink}
+              href='/course/counting-numbers'
+              ml={2}
+              variant='ghost'
+              fontWeight='600'
+            >
+              Detail
+            </Button>
+            <Button
+              background={useColorModeValue('#f0e7db', '#202023')}
+              mr={0}
+              ml={2}
+              size={{ base: 'sm', md: 'md' }}
+              as={NextLink}
+              href='/course/counting-numbers'
+            >
+              Dashboard
+            </Button>
+          </Box>
         </Box>
-      </Section>
+      </Box>
     </Container>
   );
 };
