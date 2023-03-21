7ee469f18fab3262cfdea3048b84c484292a4495
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-03-22 07:15:28 +0900

    feat(courseCard): add organisms & template

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,39 @@
 import { Box, Container, Heading, useColorModeValue } from '@chakra-ui/react';
-import CourseImage from '@/components/ui/courseCard/atoms/courseImage';
-import CourseBody from '@/components/ui/courseCard/organisms/courseBody';
+import CourseCard from '@/components/ui/courseCard/courseCard';
 
-const Home = () => {
-  const reactCourse = {
-    image: '../public/reactjs.png',
-    imageAlt: 'React Course Image',
-    title: 'Counting Numbers: 숫자 세기',
-  };
+const Home = () => (
+  <Container mb={4}>
+    <Box
+      borderRadius='lg'
+      bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      p={3}
+      mt={4}
+      mb={6}
+      textAlign='center'
+    >
+      Welcome to <b>MOP:DEV</b> courses!
+    </Box>
 
-  return (
-    <Container mb={4}>
-      <Box
-        borderRadius='lg'
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mt={4}
-        mb={6}
-        textAlign='center'
-      >
-        Welcome to <b>MOP:DEV</b> courses!
+    <Box display={{ md: 'flex' }} mb={6}>
+      <Box flexGrow={1}>
+        <Heading as='h2' variant='page-title'>
+          Courses
+        </Heading>
       </Box>
+      <Box></Box>
+    </Box>
 
-      <Box display={{ md: 'flex' }} mb={6}>
-        <Box flexGrow={1}>
-          <Heading as='h2' variant='page-title'>
-            Courses
-          </Heading>
-        </Box>
-        <Box></Box>
-      </Box>
-
-      <Box
-        maxW='container.md'
-        borderWidth='1px'
-        borderRadius='lg'
-        overflow='hidden'
-        display={{ base: 'block', sm: 'flex' }}
-        background={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        direction={{ base: 'row', sm: 'column' }}
-      >
-        <CourseImage imageSrc='reactjs.png' imageAlt={reactCourse.imageAlt} />
-        <CourseBody
-          tags={['web', 'js']}
-          level='easy'
-          dashboardLink='/course/counting-numbers'
-          detailLink='/course/counting-numbers'
-        >
-          {reactCourse.title}
-        </CourseBody>
-      </Box>
-    </Container>
-  );
-};
+    <CourseCard
+      tags={['web', 'js']}
+      level='easy'
+      dashboardLink='/course/counting-numbers'
+      detailLink='/course/counting-numbers'
+      imageSrc='reactjs.png'
+      imageAlt='React Course Image'
+    >
+      Counting Numbers: 숫자 세기
+    </CourseCard>
+  </Container>
+);
 
 export default Home;
