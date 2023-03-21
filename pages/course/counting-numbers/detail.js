4b0fc4fb41f1eddc7439b4065b593597f36d1743
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-03-22 08:45:21 +0900

    feat(counting-numbers): add schedule table

--- pages/course/counting-numbers/detail.js
+++ pages/course/counting-numbers/detail.js
@@ -3,6 +3,13 @@ import {
   Container,
   Heading,
   ListItem,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
   UnorderedList,
   useColorModeValue,
 } from '@chakra-ui/react';
@@ -33,6 +40,44 @@ const ReactCourseDetail = () => {
           </UnorderedList>
         </Box>
       </Box>
+
+      <Box
+        mt='1'
+        mb={4}
+        fontWeight='semibold'
+        fontSize={20}
+        as='h4'
+        lineHeight='tight'
+        noOfLines={1}
+        p={3}
+        flexGrow={1}
+        display='flex'
+      >
+        일정
+      </Box>
+
+      <TableContainer>
+        <Table
+          variant='simple'
+          size={{ base: 'md', md: 'lg' }}
+          colorScheme={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        >
+          <Thead>
+            <Tr>
+              <Th>Date</Th>
+              <Th>Topic</Th>
+              <Th isNumeric>materials</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>03.22</Td>
+              <Td>Vanilla JS로 숫자 세기</Td>
+              <Td isNumeric>TBD</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
