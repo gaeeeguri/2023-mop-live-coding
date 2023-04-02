03be6190242d4120f65bed116c6a221a0997476b
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-04-02 17:43:57 +0900

    style(eslint): fix eslint

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,4 +1,4 @@
-import {ChakraProviderProps, extendTheme} from '@chakra-ui/react';
+import { ChakraProviderProps, extendTheme } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
 
 const styles = {
